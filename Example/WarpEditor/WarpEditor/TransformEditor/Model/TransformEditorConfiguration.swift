14a7ab60f44b43cf49e520540813e82b828d5e06
Vitali Kurlovich <vitalikurlovich@gmail.com>
2026-02-27 00:50:25 +0400

    Adopt example from swift-mathkit

---
+++ Example/WarpEditor/WarpEditor/TransformEditor/Model/TransformEditorConfiguration.swift
@@ -0,0 +1,15 @@
+//
+//  Created by Vitali Kurlovich on 8.01.26.
+//
+
+import Observation
+
+@Observable
+final class TransformEditorConfiguration {
+    var showControlPoints = true
+    var isEditing = true
+
+    var showOutline = true
+    var showOrigin = false
+    var showGrid = true
+}
---
+++ Example/WarpEditor/WarpEditor/WrapEditorApp.swift
@@ -0,0 +1,14 @@
+//
+//  Created by Vitali Kurlovich on 26.02.26.
+//
+
+import SwiftUI
+
+@main
+struct WrapEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
